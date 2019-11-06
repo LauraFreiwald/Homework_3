0a1c0b25a25cd9d66ab9ea4c5a554261b76b9757
LauraFreiwald <57109186+LauraFreiwald@users.noreply.github.com>
2019-11-06 22:15:15 +0100

    Add files via upload

--- 3.3_Lottery.js
+++ 3.3_Lottery.js
@@ -1,31 +1,34 @@
 let secret = Math.round(Math.random() * 100);
 (function() {
 
-      let generieren= function(){
+      let generieren= function()
+      {
         let Menge = parseFloat( document.getElementById("menge").value);
         let Min = parseFloat(document.getElementById("VON").value);
         let Max = parseFloat(document.getElementById("BIS").value);
 
         
        var Zahl = [];
-
-        for( let i=0; Zahl.length < Menge; i++) // Random Zahlen reinschreiben 
+        if( Menge > Max-Min)
+         ausgabe.textContent= "Keine gültige Eingabe";
+        else
         {
-             let secret = Math.round(Math.random() * Max)+Min; // Zufallszahl im Bereich generieren
-            console.log(secret);
+            for(let i=0; Zahl.length < Menge; i++) // Random Zahlen reinschreiben 
+            {
+                let secret = Math.round(Math.random() * Max)+Min; // Zufallszahl im Bereich generieren
+                console.log(secret);
 
-            if (!Zahl.includes(secret)) // wenn Zahl noch nicht vorhanden 
-            { 
-            Zahl[i]=secret; // Zufallszahl in Array schreiben
-            } 
+                if (!Zahl.includes(secret)) // wenn Zahl noch nicht vorhanden 
+                { 
+                Zahl[i]=secret; // Zufallszahl in Array schreiben
+                } 
             
-            Zahl.sort(function(a,b) {return a.toLowerCase().localeCompare(b.toLowerCase())} );// Zahl sortieren
+                Zahl.sort(function(a,b) { return a - b});// Zahl sortieren
+                ausgabe.textContent= Zahl.toString(); // Array in String verwandeln und ausgeben 
 
-            ausgabe.textContent= Zahl.toString(); // Array in String verwandeln und ausgeben 
-
-            let Absatz = document.getElementById("ausgabe");
+                let Absatz = document.getElementById("ausgabe");
+            }  
         }
-
       } 
 
 let Generieren = document.getElementById("generieren");
